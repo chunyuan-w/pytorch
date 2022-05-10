622b329b45dbd6e80e37a4e8902f914041ab67f3
chunyuan-w <chunyuan.wu@intel.com>
2022-05-10 15:43:56 +0800

    improve compare_result shape string

--- compare_result.py
+++ compare_result.py
@@ -7,7 +7,18 @@ def parse_file(file):
     lines = []
     for i, line in enumerate(content):    
         if line.startswith("pt:"):
-            splited_element = [line.split(":")[1], line.split(":")[2].split(" ")[1]]
+            shape = line.split(":")[1]
+            dims = shape.split("_")
+            kernel = dims[4]
+            N = dims[5]
+            iC = dims[6]
+            H = dims[7]
+            W = dims[8]
+            oC = dims[9]
+            groups = dims[10]
+            
+            dims_str = "Conv+ReLU: " + "kernel=" + kernel + ", N=" + N + ", iC=" + iC + ", H=" + H + ", W=" + W + ", oC=" + oC + ", groups=" + groups
+            splited_element = [dims_str, line.split(":")[2].split(" ")[1]]
             lines.append(splited_element)
     df = pd.DataFrame(lines, columns=columns)
     df["time (us)"] = df["time (us)"].astype(float)
@@ -16,8 +27,8 @@ def parse_file(file):
 
 def compare(df1, df2):
     df = df1.merge(df2, on="shape", suffixes=["_fusion", "_no_fusion"])
-    df["time fusion / no_fusion"] = df["time (us)_fusion"] / df["time (us)_no_fusion"]
-    df["time fusion / no_fusion"] = (df["time fusion / no_fusion"] * 100).round(2).astype(str) + "%"
+    df["Gain"] = df["time (us)_no_fusion"] / df["time (us)_fusion"] - 1
+    df["Gain"] = (df["Gain"] * 100).round(2).astype(str) + "%"
     print(df)
     
 
