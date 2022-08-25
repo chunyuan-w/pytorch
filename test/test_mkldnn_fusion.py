a9f6694853957b051f811d1c32cd8b5f09677c0f
Wu, Chunyuan <chunyuan.wu@intel.com>
2022-08-25 13:09:49 +0800

    change UT order

--- test/test_mkldnn_fusion.py
+++ test/test_mkldnn_fusion.py
@@ -89,6 +89,35 @@ class TestMkldnnFusion(JitTestCase):
                     else:
                         self.assertGraphContains(graph, kind=conv_node_name)
 
+    def test_conv_eltwise(self):
+        class M(nn.Module):
+            def __init__(self, eltwise_fn, in_channels, out_channels, bias, **kwargs):
+                super(M, self).__init__()
+                self.conv = torch.nn.Conv2d(in_channels, out_channels, bias=bias, **kwargs)
+                self.eltwise = eltwise_fn
+
+            def forward(self, x):
+                x = self.conv(x)
+                x = self.eltwise(x)
+                return x
+
+        for memory_format, enabled in [
+            [torch.contiguous_format, False],
+            [torch.channels_last, True],
+        ]:
+            for eltwise_fn in [torch.relu]:
+                for bias in [True, False]:
+                    for oC in [1, 10]:
+                        m = M(eltwise_fn, 3, oC, bias, kernel_size=(3, 3)).to(memory_format=memory_format)
+                        x = torch.randn(1, 3, 224, 224).to(memory_format=memory_format)
+
+                        graph = self._check_model(m, x)
+                        if enabled:
+                            self.assertFused(graph, ['aten::conv2d', 'aten::' + eltwise_fn.__name__])
+                            self.assertGraphContainsExactly(graph, FUSION_GROUP, 1)
+                        else:
+                            self.assertGraphContains(graph, kind='aten::conv2d')
+
     def test_unsupported_conv(self):
         class M(nn.Module):
             def __init__(self, m, in_channels, out_channels, bias, **kwargs):
@@ -129,35 +158,6 @@ class TestMkldnnFusion(JitTestCase):
             x = torch.randn(input_sizes).to(memory_format=memory_format)
             graph = self._check_model(m, x, trace)
             self.assertGraphContains(graph, kind='aten::_convolution')
-
-    def test_conv_eltwise(self):
-        class M(nn.Module):
-            def __init__(self, eltwise_fn, in_channels, out_channels, bias, **kwargs):
-                super(M, self).__init__()
-                self.conv = torch.nn.Conv2d(in_channels, out_channels, bias=bias, **kwargs)
-                self.eltwise = eltwise_fn
-
-            def forward(self, x):
-                x = self.conv(x)
-                x = self.eltwise(x)
-                return x
-
-        for memory_format, enabled in [
-            [torch.contiguous_format, False],
-            [torch.channels_last, True],
-        ]:
-            for eltwise_fn in [torch.relu]:
-                for bias in [True, False]:
-                    for oC in [1, 10]:
-                        m = M(eltwise_fn, 3, oC, bias, kernel_size=(3, 3)).to(memory_format=memory_format)
-                        x = torch.randn(1, 3, 224, 224).to(memory_format=memory_format)
-
-                        graph = self._check_model(m, x)
-                        if enabled:
-                            self.assertFused(graph, ['aten::conv2d', 'aten::' + eltwise_fn.__name__])
-                            self.assertGraphContainsExactly(graph, FUSION_GROUP, 1)
-                        else:
-                            self.assertGraphContains(graph, kind='aten::conv2d')
 
 
 if __name__ == "__main__":
