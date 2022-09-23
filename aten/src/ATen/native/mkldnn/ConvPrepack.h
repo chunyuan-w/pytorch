20872655fc8b1a9808015224ee82eb187ac96310
chunyuan <chunyuan.wu@intel.com>
2022-09-23 11:53:14 +0000

    add linear eltwise fusion kernel

--- aten/src/ATen/native/mkldnn/ConvPrepack.h
+++ aten/src/ATen/native/mkldnn/ConvPrepack.h
@@ -40,6 +40,14 @@ Tensor run(ContextConv& context, const Tensor& input);
 
 void run(ContextConv& context, const Tensor& input, void* output);
 
+Tensor linear_eltwise_run(
+    const Tensor& input,
+    const Tensor& weight_t,
+    const c10::optional<Tensor>& bias_opt,
+    std::string attr,
+    std::vector<c10::optional<at::Scalar>> scalars,
+    c10::optional<std::string> algorithm);
+
 } // namespace convolution
 } // namespace internal
 } // namespace mkldnn
