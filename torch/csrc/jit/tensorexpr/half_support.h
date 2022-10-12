c2c645e118a7f3f126d3444288e43ed8c962952c
Wu, Chunyuan <chunyuan.wu@intel.com>
2022-10-12 17:04:09 +0800

    fix the missing cast in half_support

--- torch/csrc/jit/tensorexpr/half_support.h
+++ torch/csrc/jit/tensorexpr/half_support.h
@@ -83,6 +83,11 @@ class HalfRewriter : public IRMutator {
       inserted_half_casts_.insert(new_val);
     }
 
+    if (isHalf(v->buf()->dtype().scalar_type()) && !isHalf(newType.scalar_type())) {
+      new_val = alloc<Cast>(v->buf()->dtype(), new_val);
+      inserted_half_casts_.insert(new_val);
+    }
+
     v->set_value(new_val);
     return v;
   }
