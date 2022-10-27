9bc9562ec63e384dd96521ed810d5dec07532000
chunyuan <chunyuan.wu@intel.com>
2022-10-27 10:36:28 +0000

    fix lint

--- torch/_inductor/codegen/wrapper.py
+++ torch/_inductor/codegen/wrapper.py
@@ -565,7 +565,13 @@ class CppWrapperCodeGen(WrapperCodeGen):
                 result.writeline("return; }''' )")
 
         result.writeline(
-            "module = load_inline(name='inline_extension', cpp_sources=[wrapper], functions=['call'], extra_cflags=['-DCPU_CAPABILITY_AVX2 -march=native -O3 -ffast-math -fno-finite-math-only -fopenmp'])"
+            """
+module = load_inline(
+    name='inline_extension',
+    cpp_sources=[wrapper],
+    functions=['call'],
+    extra_cflags=['-DCPU_CAPABILITY_AVX2 -march=native -O3 -ffast-math -fno-finite-math-only -fopenmp'])
+            """
         )
         result.writeline("call = module.call")
         self.add_benchmark_harness(result)
