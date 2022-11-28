df5d1090e2bd24ee728e66a9e26ddc859cabbf34
Wu, Chunyuan <chunyuan.wu@intel.com>
2022-11-28 16:25:42 +0800

    Add a cpp wrapper for Inductor
    ghstack-source-id: 4f958a026d8461d45ae435a5548638659a0dfb52
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88167

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -4677,6 +4677,22 @@ class CommonTemplate:
             [torch.randn((4, 2)), torch.randn((4))],
         )
 
+    @patch.object(config, "cpp_wrapper", True)
+    def test_cpp_wrapper(self):
+        device = "cpu"
+        for name in [
+            "test_as_strided",  # buffer reuse
+            "test_cat",  # alias
+            "test_relu",  # multiple inputs
+            "test_transpose",  # multiple outputs, buffer clear
+            "test_upsample_bilinear2d_b",  # single input, single output
+        ]:
+            test_name = f"{name}_{device}"
+            assert hasattr(self, test_name), "undefined function"
+            func = getattr(self, test_name)
+            assert callable(func), "not a callable"
+            func()
+
 
 if HAS_CPU:
 
