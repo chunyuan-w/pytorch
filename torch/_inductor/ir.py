fd5218cc81747fd9d3880fd1d35b8bb90ae0447a
chunyuan <chunyuan.wu@intel.com>
2022-12-06 10:23:51 +0000

    weight prepack: refactor ir.py size calculation

--- torch/_inductor/ir.py
+++ torch/_inductor/ir.py
@@ -3399,6 +3399,55 @@ def _prepare_convolution_fusion_create(
     return inputs, constant_args, kernel_layout, req_stride_order
 
 
+# Port from aten/src/ATen/native/ConvUtils.h: _conv_input_size
+def _conv_input_size(
+    output_size,
+    weight_size,
+    padding,
+    output_padding,
+    stride,
+    dilation,
+    groups
+):
+    assert len(output_size) == len(weight_size), "Expect input dim == weight dim"
+    dim = len(output_size)
+    assert dim > 2, "Expect input dim > 2"    
+    
+    BATCH_DIM = 0
+    WEIGHT_INPUT_CHANNELS_DIM = 1
+    input_size = []
+    input_size.append(output_size[BATCH_DIM])
+    input_size.append(weight_size[WEIGHT_INPUT_CHANNELS_DIM] * groups)
+    for d in range(2, dim):
+        kernel = (weight_size[d] - 1) * dilation[d - 2] + 1;
+        input_size_d = (output_size[d] - 1) * stride[d - 2] - (padding[d - 2] * 2) + kernel + output_padding[d - 2];            
+        input_size.append(input_size_d)
+    return input_size
+
+
+# prepacked weight size:
+#   Groups > 1:  [g*o, i/g, ...]
+#   Groups == 1: [o, i, ...]
+# Returns original weight size in:
+#   [i, o, ...]
+def _original_deconv_weight_size(
+    prepacked_weight,
+    groups,
+):
+    prepacked_weight_size = prepacked_weight.size()
+    dim = len(prepacked_weight_size)
+    assert dim > 2, "Expect weight dim > 2"
+    if groups > 1:
+        weight_size = []
+        weight_size.append(prepacked_weight_size[1] * groups)
+        weight_size.append(prepacked_weight_size[0] / groups)
+        for d in range(2, dim):
+            weight_size.append(prepacked_weight_size[d])
+    else:
+        weight_size = prepacked_weight.transpose(0, 1).size()
+    return weight_size
+
+
 def _prepare_convolution_transpose_fusion_create(
     cls,
     x: "TensorBox",
@@ -3426,32 +3475,9 @@ def _prepare_convolution_transpose_fusion_create(
         x_fake = ir_node_to_tensor(x, guard_shape=True)
         weight_fake = ir_node_to_tensor(weight, guard_shape=True)
 
+        weight_size = _original_deconv_weight_size(weight_fake, groups)
         input_size = x_fake.size()
-        dim = len(input_size)
-
-        assert dim > 2, "Expect input size > 2"
-        # weight is packed: weight is in OIHW or G * O, I/G, ...
-        if groups > 1:
-            weight_size = []
-            weight_size.append(weight_fake.size()[1] * groups)
-            weight_size.append(weight_fake.size()[0] / groups)
-            for d in range(2, dim):
-                weight_size.append(weight_fake.size()[d])
-        else:
-            weight_size = weight_fake.transpose(0, 1).size()
-        
-        assert len(input_size) == len(weight_size), "Expect input size == weight size"
-        
-
-        BATCH_DIM = 0
-        WEIGHT_INPUT_CHANNELS_DIM = 1
-        output_size = []
-        output_size.append(input_size[BATCH_DIM])
-        output_size.append(weight_size[WEIGHT_INPUT_CHANNELS_DIM] * groups)
-        for d in range(2, dim):
-            kernel = (weight_size[d] - 1) * dilation[d - 2] + 1;
-            output_size_d = (input_size[d] - 1) * stride[d - 2] - (padding[d - 2] * 2) + kernel + output_padding[d - 2];            
-            output_size.append(output_size_d)
+        output_size = _conv_input_size(input_size, weight_size, padding, output_padding, stride, dilation, groups)
 
         req_stride_order = [0] + list(reversed(range(1, len(stride) + 1)))
         req_stride_order = [len(req_stride_order)] + req_stride_order
