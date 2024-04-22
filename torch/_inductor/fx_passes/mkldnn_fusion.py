9a52429b4487e6839c97343ec7bd688512a51793
chunyuan <chunyuan.wu@intel.com>
2024-04-22 09:31:58 +0000

    [TEST ONLY] use oneDNN for FP32 GEMM

--- torch/_inductor/fx_passes/mkldnn_fusion.py
+++ torch/_inductor/fx_passes/mkldnn_fusion.py
@@ -1308,6 +1308,9 @@ if torch._C._has_mkldnn:
                     torch.bfloat16,
                     torch.float16,
                 )
+                # Test to use oneDNN for FP32 GEMM
+                is_lp_weight = True
+                
                 batch_size = input.meta.get("val").shape[0]
                 if has_free_symbols(batch_size):
                     assert is_lp_weight or mkldnn._is_mkldnn_acl_supported(), (
