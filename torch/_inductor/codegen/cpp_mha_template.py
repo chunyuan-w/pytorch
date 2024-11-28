2b15b3788c81ceec8416a6b66bb52e451865f440
Wu, Chunyuan <chunyuan.wu@intel.com>
2024-11-28 06:06:15 -0500

    merge 2 other_ptr dicts into 1

--- torch/_inductor/codegen/cpp_mha_template.py
+++ torch/_inductor/codegen/cpp_mha_template.py
@@ -371,8 +371,7 @@ class CppMHATemplate(CppTemplate):
         self.kernel_input_name_to_buffer = kernel_input_name_to_buffer
         self.score_mod_other_buffers = self.input_nodes[5 + self.other_buffer_input_offset:5 + self.other_buffer_input_offset + self.len_score_other] if self.has_other_buffer else None
         self.mask_mod_other_buffers=self.input_nodes[5 + self.other_buffer_input_offset + self.len_score_other:] if self.has_other_buffer else None
-        self.other_ptr_to_name = {}
-        self.other_ptr_to_arg = {}
+        self.other_ptr_data = {}
 
     def update_kernel_args(self, kernel_args):
         kernel_args.update(self.kernel_input_name_to_buffer)
@@ -399,14 +398,15 @@ class CppMHATemplate(CppTemplate):
         for i in range(length):
             pointer = f"in_ptr{start_ptr + start_offset + i}"
             buffer_key = f"{buf_list}_{i}"
-            if pointer not in self.other_ptr_to_name:
-                self.other_ptr_to_name[pointer] = get_arg_name(buffer_key)
-            if pointer not in self.other_ptr_to_arg:
-                self.other_ptr_to_arg[pointer] = get_arg(buffer_key)
+            if pointer not in self.other_ptr_data:
+                self.other_ptr_data[pointer] = (
+                    get_arg_name(buffer_key),
+                    get_arg(buffer_key),
+                )
         
         return "\n".join(
             f"auto {ptr} = {name};"
-            for ptr, name in self.other_ptr_to_name.items()
+            for ptr, (name, _) in self.other_ptr_data.items()
         )
 
     def modification(self, subgraph_buffer, output_name, output_idx):
@@ -432,8 +432,8 @@ class CppMHATemplate(CppTemplate):
         if self.has_other_buffer:
             start_ptr = 5
             kernel_input_args.update({
-                name: ptr 
-                for ptr, name in self.other_ptr_to_arg.items()
+                arg: ptr 
+                for ptr, (_, arg) in self.other_ptr_data.items()
             })
 
         kernel_output_args = {
